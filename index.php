fc839286cc2d5f1a21de115031e4f424833bd058
Evgeniy Rodchenkov <esrodchenkov@gmail.com>
2016-02-25 00:15:11 +0300

    !

--- index.php
+++ index.php
@@ -2,20 +2,17 @@
 session_start();
 define ( 'READFILE', true );
 ?>
-
 <?php
 include ("$_SERVER[DOCUMENT_ROOT]/template/head.php");
 include ("$_SERVER[DOCUMENT_ROOT]/auth/auth.php");
 include ("$_SERVER[DOCUMENT_ROOT]/template/main_menu.php");
 ?>
-
-<div class="content">
+	<div class="content">
 <?php
 include ("$_SERVER[DOCUMENT_ROOT]/content/content_index.php");	
 ?>
-<!-- end .content --></div>
-
+<!-- end .content -->
+	</div>
 <?php
-
 include ("$_SERVER[DOCUMENT_ROOT]/template/head_end.php");
 ?>
